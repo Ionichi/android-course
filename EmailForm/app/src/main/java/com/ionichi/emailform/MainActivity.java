35d21b11f0293a36d7b3c07a2619be0f12bd4e87
ionichi <feryandi2303@gmail.com>
2025-03-20 20:43:41 +0700

    feat: email form notification permission

--- EmailForm/app/src/main/java/com/ionichi/emailform/MainActivity.java
+++ EmailForm/app/src/main/java/com/ionichi/emailform/MainActivity.java
@@ -1,15 +1,30 @@
 package com.ionichi.emailform;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button _sendButton;
     private EditText _recipientEditText, _subjectEditText, _messageEditText;
+    private static final int RC_NOTIFICATION = 23;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (!checkNotificationPermission()) {
+            askNotificationPermission();
+        }
 
         initSendButton();
     }
@@ -42,25 +62,77 @@ public class MainActivity extends AppCompatActivity {
         _sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // min sdk 26 (android 8)
-                NotificationChannel channel = new NotificationChannel("ioChannel", "IO", NotificationManager.IMPORTANCE_DEFAULT);
+                if (checkNotificationPermission()) {
+                    // min sdk 26 (android 8)
+                    NotificationChannel channel = new NotificationChannel("ioChannel", "IO", NotificationManager.IMPORTANCE_DEFAULT);
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext())
-                        .setSmallIcon(R.drawable.logo_yorionic)
-                        .setContentTitle("Email from " + _recipientEditText.getText())
-                        .setContentText(_subjectEditText.getText())
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(_subjectEditText.getText() + "\n" + _recipientEditText.getText() + ": " + _messageEditText.getText()))
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .setChannelId(channel.getId());
-//                        notif gambar
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext())
+                            .setSmallIcon(R.drawable.logo_yorionic)
+                            .setContentTitle("Email from " + _recipientEditText.getText())
+                            .setContentText(_subjectEditText.getText())
+                            .setStyle(new NotificationCompat.BigTextStyle()
+                                    .bigText(_subjectEditText.getText() + "\n" + _recipientEditText.getText() + ": " + _messageEditText.getText()))
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setChannelId(channel.getId());
+//                        notification attach image
 //                        .setStyle((new NotificationCompat.BigPictureStyle()
 //                                .bigPicture(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.logo_yorionic))))
 
-                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                manager.createNotificationChannel(channel);
-                manager.notify(1, builder.build());
+                    NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    manager.createNotificationChannel(channel);
+                    manager.notify(1, builder.build());
+                } else {
+                    showPermissionDeniedDialog();
+                }
             }
         });
+    }
+
+    private void askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, RC_NOTIFICATION);
+            }
+        }
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+//        if (requestCode == RC_NOTIFICATION) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Toast.makeText(this, "Allowed", Toast.LENGTH_SHORT).show();
+//            } else {
+//                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.POST_NOTIFICATIONS)) {
+////                    Toast.makeText(this, "Denied", Toast.LENGTH_SHORT).show();
+//                    showPermissionDeniedDialog();
+//                }
+//            }
+//        }
+//    }
+
+    // show dialog when notification permission is denied permanent
+    private void showPermissionDeniedDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Izin Diperlukan")
+            .setMessage("Aplikasi memerlukan izin notifikasi. Silakan aktifkan secara manual di Pengaturan.")
+            .setPositiveButton("Buka Pengaturan", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+            })
+            .setNegativeButton("Batal", null)
+            .show();
+    }
+
+    private boolean checkNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
     }
 }
