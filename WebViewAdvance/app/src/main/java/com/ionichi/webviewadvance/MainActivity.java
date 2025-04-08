903b5e32ecec9241c9362bb5a8dbe443d111907b
ionichi <feryandi2303@gmail.com>
2025-04-08 17:20:02 +0700

    feat: webview advance and update webview project (add icon button)

---
+++ WebViewAdvance/app/src/main/java/com/ionichi/webviewadvance/MainActivity.java
@@ -0,0 +1,48 @@
+package com.ionichi.webviewadvance;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView _webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        _webView = findViewById(R.id.webView);
+
+        WebViewClient webViewClient = new WebViewClient();
+        _webView.setWebViewClient(webViewClient);
+
+        WebChromeClient webChromeClient = new WebChromeClient();
+        _webView.setWebChromeClient(webChromeClient);
+
+        WebSettings webSettings = _webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        WebAppInterface webAppInterface = new WebAppInterface(this, MainActivity.this);
+        _webView.addJavascriptInterface(webAppInterface, "Android");
+
+        String url = "https://stmikpontianak.cloud/011100862/webview_lanjutan.html";
+        _webView.loadUrl(url);
+    }
+}
