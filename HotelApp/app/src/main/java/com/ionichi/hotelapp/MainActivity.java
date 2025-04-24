8e4fc97c06c4bdac3c602cfa82eede7d7b6c0743
ionichi <feryandi2303@gmail.com>
2025-04-24 17:05:30 +0700

    chore: update hotel app (add image)

--- HotelApp/app/src/main/java/com/ionichi/hotelapp/MainActivity.java
+++ HotelApp/app/src/main/java/com/ionichi/hotelapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ionichi.hotelapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView _imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        _imageView = findViewById(R.id.imageView1);
+
+        String imageUrl = "https://i.pinimg.com/736x/2d/3f/23/2d3f230e477a95d2ea1ed8bc603d7b83.jpg";
+        Picasso.with(this).load(imageUrl).into(_imageView);
     }
 }
