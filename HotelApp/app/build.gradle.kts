de9d2d75470e903ae40d60e5dba6e83edee6fdb9
ionichi <feryandi2303@gmail.com>
2025-05-27 16:42:07 +0700

    docs: give command on picasso package/libs

--- HotelApp/app/build.gradle.kts
+++ HotelApp/app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.picasso)
+    implementation(libs.picasso) // image loader
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
